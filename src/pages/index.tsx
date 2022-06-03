a768733cbcb1cbc124cf3888027aa9f6c8d09307
Fernando Bosco <fbosco@dlocal.com>
2022-06-03 00:41:45 -0300

    initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,27 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Box, Heading } from "@chakra-ui/react";
+import Player from "@vimeo/player";
+import React, { useEffect } from "react";
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+export default function Index() {
+  useEffect(() => {
+    var options01 = {
+      id: 715848386,
+      width: 1000,
+    };
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text color="text">
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
+    var video01Player = new Player("{video01_name}", options01);
 
-      <List spacing={3} my={0} color="text">
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+    video01Player.setVolume(0);
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+    video01Player.on("play", function () {
+      console.log("Played the first video");
+    });
+  }, []);
 
-export default Index
+  return (
+    <Box p={5}>
+      <Heading>vimeo with nextjs</Heading>
+      <Box id="{video01_name}"></Box>
+    </Box>
+  );
+}
